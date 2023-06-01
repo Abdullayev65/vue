863fddc742e233a8637a5dbb7b6383319ce725a3
Abdullayev65 <abdullayev65abdulloh@gmail.com>
2023-06-01 10:17:59 +0500

    b1s65 vite-project-temp

---
+++ samar_badriddinov/vite-project-temp/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h1>
+      H W ! {{counter}}
+    </h1>
+    <button v-on:click="onIncrement" class="">
+      Add
+    </button>
+    <button v-on:click="onDecrement" class="">
+      Del
+    </button>
+    <App v-if="counter>3" />
+  </div>
+
+</template>
+
+<script>
+import App from "@/App.vue";
+export default {
+  data() {
+    return {
+      counter: 1
+    }
+  },
+  methods: {
+    onIncrement() {
+      this.counter++
+    },
+    onDecrement() {
+      this.counter--
+    }
+  },
+  components:{
+    App
+  }
+}
+</script>
+
+<style></style>
+
